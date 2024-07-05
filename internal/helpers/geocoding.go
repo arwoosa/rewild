848589bdfe13a813d979556e1bd2370eff158070
fortunio <10980185+fortunio@users.noreply.github.com>
2024-07-05 16:47:09 +0700

    Changes to event related objects

--- internal/helpers/geocoding.go
+++ internal/helpers/geocoding.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"fmt"
+	"math"
 	"oosa_rewild/internal/config"
 
 	"github.com/gin-gonic/gin"
@@ -46,4 +47,25 @@ func GoogleMapsElevation(c *gin.Context, lat float64, lng float64) maps.Elevatio
 		fmt.Println("ERROR...", err.Error())
 	}
 	return result[0]
+}
+
+func Haversine(lat1 float64, lon1 float64, lat2 float64, lon2 float64) float64 {
+
+	fmt.Println(lat1, lon1, lat2, lon2)
+	var R float64 = 6371
+	var x1 = lat2 - lat1
+	dLat := toRad(x1)
+	var x2 = lon2 - lon1
+	dLon := toRad(x2)
+
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Cos(toRad(lat1))*math.Cos(toRad(lat2))*
+			math.Sin(dLon/2)*math.Sin(dLon/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	d := R * c
+	return d
+}
+
+func toRad(deg float64) float64 {
+	return deg * (math.Pi / 180)
 }
--- internal/models/eventReferenceLinks.go
+++ internal/models/eventReferenceLinks.go
@@ -3,10 +3,11 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type EventReferenceLinks struct {
-	EventReferenceLinksId        primitive.ObjectID `bson:"_id,omitempty" json:"event_reference_links_id"`
-	EventReferenceLinksEvent     primitive.ObjectID `bson:"event_reference_links_event,omitempty" json:"event_reference_links_event,omitempty"`
-	EventReferenceLinksLink      string             `bson:"event_reference_links_link,omitempty" json:"event_reference_links_link,omitempty"`
-	EventReferenceLinksTitle     string             `bson:"event_reference_links_title,omitempty" json:"event_reference_links_title,omitempty"`
-	EventReferenceLinksCreatedAt primitive.DateTime `bson:"event_reference_links_created_at,omitempty" json:"event_reference_links_created_at,omitempty"`
-	EventReferenceLinksCreatedBy primitive.ObjectID `bson:"event_reference_links_created_by,omitempty" json:"event_reference_links_created_by,omitempty"`
+	EventReferenceLinksId            primitive.ObjectID `bson:"_id,omitempty" json:"event_reference_links_id"`
+	EventReferenceLinksEvent         primitive.ObjectID `bson:"event_reference_links_event,omitempty" json:"event_reference_links_event,omitempty"`
+	EventReferenceLinksLink          string             `bson:"event_reference_links_link,omitempty" json:"event_reference_links_link,omitempty"`
+	EventReferenceLinksTitle         string             `bson:"event_reference_links_title,omitempty" json:"event_reference_links_title,omitempty"`
+	EventReferenceLinksCreatedAt     primitive.DateTime `bson:"event_reference_links_created_at,omitempty" json:"event_reference_links_created_at,omitempty"`
+	EventReferenceLinksCreatedBy     primitive.ObjectID `bson:"event_reference_links_created_by,omitempty" json:"event_reference_links_created_by,omitempty"`
+	EventReferenceLinksCreatedByUser *UsersAgg          `bson:"event_reference_links_created_by_user,omitempty" json:"event_reference_links_created_by_user,omitempty"`
 }
