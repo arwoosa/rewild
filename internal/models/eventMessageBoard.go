c2d5579fb29ce428e2c801f67e4ee1be66686304
fortunio <10980185+fortunio@users.noreply.github.com>
2024-10-20 22:43:34 +0700

    1.1.21
    - CRUD /event/{eventId}/announcement Use one body structure
    - POST rewilding name: 30 chars, images: max 3, reference link: max 3
    - GET rewilding-searchText, GET rewilding-searchNearby handle if no places found
    - All rewilding create will be run across the achievement database to get 百岳, 小百岳, 保護區 data. Creation of event will automatically add this flag to the data

--- internal/models/eventMessageBoard.go
+++ internal/models/eventMessageBoard.go
@@ -15,3 +15,16 @@ type EventMessageBoard struct {
 	EventMessageBoardIsPinned      *int               `bson:"event_message_board_is_pinned,omitempty" json:"event_message_board_is_pinned"`
 	EventMessageBoardCreatedByUser *UsersAgg          `bson:"event_message_board_created_by_user,omitempty" json:"event_message_board_created_by_user,omitempty"`
 }
+type EventAnnouncement struct {
+	EventMessageBoardId            primitive.ObjectID `bson:"_id,omitempty" json:"event_announcement_id"`
+	EventMessageBoardEvent         primitive.ObjectID `bson:"event_message_board_event,omitempty" json:"event_announcement_event"`
+	EventMessageBoardBaseMessage   string             `bson:"event_message_board_base_message,omitempty" json:"event_announcement_base_message,omitempty"`
+	EventMessageBoardStatus        int                `bson:"event_message_board_status,omitempty" json:"event_announcement_status,omitempty"`
+	EventMessageBoardCategory      string             `bson:"event_message_board_category,omitempty" json:"event_announcement_category,omitempty"`
+	EventMessageBoardAnnouncement  string             `bson:"event_message_board_announcement,omitempty" json:"event_announcement_announcement,omitempty"`
+	EventMessageBoardMessageId     primitive.ObjectID `bson:"event_message_board_message_id,omitempty" json:"event_announcement_message_id"`
+	EventMessageBoardCreatedBy     primitive.ObjectID `bson:"event_message_board_created_by,omitempty" json:"event_announcement_created_by"`
+	EventMessageBoardCreatedAt     primitive.DateTime `bson:"event_message_board_created_at,omitempty" json:"event_announcement_created_at"`
+	EventMessageBoardIsPinned      *int               `bson:"event_message_board_is_pinned,omitempty" json:"event_announcement_is_pinned"`
+	EventMessageBoardCreatedByUser *UsersAgg          `bson:"event_message_board_created_by_user,omitempty" json:"event_announcement_created_by_user,omitempty"`
+}
